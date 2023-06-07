ca165e3ed4585cd2e27a186d2cddc40234f0be17
Camille Lenormand <camille.lenormand02@gmail.com>
2023-06-07 08:12:47 +0200

    update status in models

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,7 @@
 class Cart<ApplicationRecord
   validates :user_id, presence: true
-
+  enum status: { in_progress: 0, validated: 1, paid: 2, cancelled: 3, trashed: 4 }
+  
   has_many :cart_items, dependent: :destroy
   belongs_to :user
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
+  validates :user_id, presence: true
+  validates :status, presence: true
+  enum status: { in_progress: 0, validated: 1, paid: 2, cancelled: 3, trashed: 4 }
+  
   belongs_to :user
   has_many :order_items, dependent: :destroy
 end
